5fb942e91e69ca7a4a4d149dc0ba1754a7365386
MiguelED <migueled2695@gmail.com>
2021-04-03 00:42:04 -0600

    Chat component e interfaz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { ChatComponent } from './componenets/chat/chat.component';
+
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
     AngularFirestoreModule
---
+++ src/app/componenets/chat/chat.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styles: [
+  ]
+})
+export class ChatComponent implements OnInit {
+  mensaje:string="";
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  enviarMensaje(){
+    console.log(this.mensaje);
+  }
+}
